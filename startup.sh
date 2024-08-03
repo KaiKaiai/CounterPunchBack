ee1675c31494f7b29c978890dcb4ac5a2e0e3bc1
KaiKaiai <greekstylemix@gmail.com>
2024-08-04 03:35:55 +1000

    feat(change): transfer for server side rendering roboflow

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Update package list and install necessary libraries
-apt-get update && apt-get install -y libgl1-mesa-glx
-
-# Start the application
-gunicorn --chdir /home/site/wwwroot run:app
