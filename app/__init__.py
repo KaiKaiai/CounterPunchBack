8cd5515f95c4ce079fe1b4f5f3874dcc6234ab9b
KaiKaiai <greekstylemix@gmail.com>
2024-08-03 15:38:38 +1000

    update cors for live

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS to allow requests from the React frontend
+CORS(app, resources={r"/api/*": {"origins": "https://https://counter-punch.vercel.app/"}})
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
