e7466fa45e402a23ba79b07fa1038ef9666a4c76
KaiKaiai <greekstylemix@gmail.com>
2024-08-03 18:35:55 +1000

    remove azure for relaunch

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update package list and install necessary libraries
-apt-get update && apt-get install -y libgl1-mesa-glx
-
-# Activate virtual environment
-source /home/site/wwwroot/venv/bin/activate
-
-# Start the application
-gunicorn --chdir /home/site/wwwroot run:app
